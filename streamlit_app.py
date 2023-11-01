9327cce125e2c2f4941de2d4cac30f9bc04293b0
Piesang007 <149340963+Piesang007@users.noreply.github.com>
2023-11-02 12:21:50 +1300

    Update streamlit_app.py
    moved fruity vice to bottom

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,7 @@ import streamlit
 import pandas
 import requests
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
@@ -19,6 +18,9 @@ streamlit.text("🥑🍞 Avocado Toast");
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇');
 # Let's put a pick list here so they can pick the fruit they want to include 
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries']);
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
 
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
