3303e81ec6bd7ec22aed60a0a8e83aa940fdd2ce
Piesang007 <149340963+Piesang007@users.noreply.github.com>
2023-11-03 15:04:14 +1300

    Update streamlit_app.py
    typo 
    close cursors

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,12 +52,13 @@ def get_fruit_load_list():
 if streamlit.button('Get Fruit Load List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data_rows = get_fruit_load_list()
+    my_cnx.close()
     streamlit.dataframe(my_data_rows)
 
 #Allow end user to add a fruit
 def insert_row_snowflake(new_fruit):
     with my_cnx.cursor() as my_cur:
-         my_cur.execute("insert into fruit_load)list values ('" + new_fruit +"')")
+         my_cur.execute("insert into fruit_load_list values ('" + new_fruit +"')")
          return "Thanks for adding " + new_fruit
 
 add_my_fruit = streamlit.text_input('What Fruit would you like to add?')
@@ -65,5 +66,6 @@ if streamlit.button('Add a Fruit to the list'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     back_from_function = insert_row_snowflake(add_my_fruit)
     streamlit.text(back_from_function)
+    my_cnx.close()
 
 
